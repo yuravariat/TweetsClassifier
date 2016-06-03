7b5118364a40ee0770efd6733e0012638e9479a6
yuravariat <yura.variat@gmail.com>
2016-06-04 02:16:14 +0300

    pos transformer (not ready yet)

--- code_examples/pos_example.py
+++ code_examples/pos_example.py
@@ -1,7 +1,18 @@
 import nltk
 
 #test pos
-# nltk.download()
-ttt = nltk.pos_tag(['is', 'table'])
+#nltk.download()
+
+words = ['is', 'table','apple','are','tree','run','running','sit']
+pos_window = nltk.pos_tag(words)
+
+tag_fd = nltk.FreqDist(tag for (word, tag) in pos_window)
+frequencies = tag_fd.most_common()
+
+jhgjhj = nltk.help.upenn_tagset()
+tttt = nltk.help.upenn_tagset('VB')
+from nltk.data import load
+tagdict = load('help/tagsets/upenn_tagset.pickle')
+kjhkj = tagdict.keys()
 
 test = 'stop'
